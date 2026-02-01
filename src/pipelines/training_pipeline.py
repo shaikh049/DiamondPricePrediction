33dd611d525a90ef58f3324b3697d030325ecc1a
unknown <sh.ashfaqueme49@gmail.com>
2026-02-02 01:14:29 +0530

    3rd Data transformation done

--- src/pipelines/training_pipeline.py
+++ src/pipelines/training_pipeline.py
@@ -5,6 +5,7 @@ from src.exception import CustomException
 import pandas as pd
 
 from src.components.data_ingestion import DataIngestion
+from src.components.data_transformation import DataTransformation
 
 if __name__ == "__main__":
     try:
@@ -12,6 +13,17 @@ if __name__ == "__main__":
         train_data_path, test_data_path = obj.initiate_data_ingestion()
         print(f"Train Path: {train_data_path}")
         print(f"Test Path: {test_data_path}")
+
+        data_transformation=DataTransformation()
+
+        train_arr,test_arr,_=data_transformation.initiate_data_tranformation(train_data_path,test_data_path)
+
+
     except Exception as e:
         logging.error("Exception occurred in training pipeline")
         raise CustomException(e, sys)
+    
+
+
+    
+
